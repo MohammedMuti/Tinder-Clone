e1929a64c7385f62f063d07558a6df031d0987b2
Mohammed Muti <mohammedmuti74@gamil.com>
2023-03-03 12:19:40 +0530

    server deployed

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8001",
+  baseURL: "https://tinder-clone-backend-7gbc.onrender.com",
 });
 
 export default instance;
